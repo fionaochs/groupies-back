dcbeb6dd184d00109dba018f27566f13917e4e3a
Logan Scott <logantscott@gmail.com>
2020-03-04 09:31:50 -0800

    added tm_id to saved

--- server.js
+++ server.js
@@ -125,11 +125,11 @@ app.post('/api/me/saved', async(req, res) => {
     try {
         console.log(req.body);
         const newSaved = await client.query(`
-            INSERT into saved (user_id, name, images, genre, start_date, tickets_url, city, state, price_min, price_max, lat, long)
-            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+            INSERT into saved (user_id, name, images, genre, start_date, tickets_url, city, state, price_min, price_max, lat, long, tm_id)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
             RETURNING *;
         `,
-        [req.userId, req.body.name, req.body.images, req.body.genre, req.body.start_date, req.body.tickets_url, req.body.city, req.body.state, req.body.price_min, req.body.price_max, req.body.longitude, req.body.latitude]);
+        [req.userId, req.body.name, req.body.images, req.body.genre, req.body.start_date, req.body.tickets_url, req.body.city, req.body.state, req.body.price_min, req.body.price_max, req.body.longitude, req.body.latitude, req.body.tm_id]);
         res.json(newSaved.rows[0]);
     }
     catch (err) {
